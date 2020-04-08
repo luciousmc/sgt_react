9f44357a54ba8edd5be6df765b31a729e8af9d69
Marlon <luciousmc@icloud.com>
2020-04-08 01:08:08 -0700

    webpack updated changed folder structure to server and client folders

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,31 @@
+const path = require('path');
+
+const srcFolder = path.resolve(__dirname, 'client');
+const publicFolder = path.resolve(__dirname, 'server/public');
+
 module.exports = {
   mode: 'none',
   resolve: {
     extensions: ['.js', '.jsx']
   },
-  rules: [
-    {
-      test: /\.jsx?$/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          plugins: [
-            '@babel/plugin-transform-react-jsx'
-          ]
+  entry: './client',
+  output: {
+    path: publicFolder
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: srcFolder,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: [
+              '@babel/plugin-transform-react-jsx'
+            ]
+          }
         }
       }
-    }
-  ]
+    ]
+  }
 };
