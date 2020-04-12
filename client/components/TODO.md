546cca23d425506ed82f540c21ae82bff3f4072e
Marlon <luciousmc@icloud.com>
2020-04-11 23:56:54 -0700

    adds stepper for registration form

---
+++ client/components/TODO.md
@@ -0,0 +1,21 @@
+# TODOS
+
+## Stepper Login
+
+### Information needed for Database
+
+- [ ] First Name
+- [ ] Last Name
+- [ ] Phone Number
+- [ ] Address
+  - [ ] Street
+  - [ ] City
+  - [ ] State
+  - [ ] Zipcode
+- [ ] D.O.B.
+- [ ] S.S.N.
+- [ ] Email Address
+- [ ] CHHA Certificat Number
+- [ ] CHHA Certificate Expiration Date
+- [ ] CNA Certificate Number
+- [ ] CNA Certificate Expiration Date
--- client/components/register.jsx
+++ client/components/register.jsx
@@ -1,8 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Paper, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    marginTop: theme.spacing(5),
+    padding: 20
+  }
+}));
+
+const getSteps = () => {
+  return [
+    'Personal Information',
+    'Work Information',
+    'Certifications'
+  ];
+}
+
+const getStepContent = stepId => {
+  switch(stepId) {
+    case 0:
+      return <h1>Personal Information</h1>
+    case 1:
+      return <h1>Work Information</h1>
+    case 2:
+      return <h1>Certifications</h1>
+    default:
+      return <h1>Invalid Step</h1>
+  }
+}
 
 const Register = () => {
+  const classes = useStyles();
+  const [ activeStep, setActiveStep ] = useState(0);
+  const steps = getSteps();
+
   return(
-    <h1>Register Screen</h1>
+    <Paper className={classes.root}>
+      <Typography variant='h4' align='center' gutterBottom>
+        Register
+      </Typography>
+      
+    </Paper>
   );
 };
 
